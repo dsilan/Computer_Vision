00acfc65ce4a503cf3778755d2c45c3d91b05c78
dsilan <dorsinsilan.isik@ogr.iu.edu.tr>
2020-01-03 21:07:59 +0300

    added erosions and dilataions

--- opencv_tutorial/opencv_tutorial_2.py
+++ opencv_tutorial/opencv_tutorial_2.py
@@ -9,7 +9,8 @@ image = cv2.imread(args["image"])
 gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 edged = cv2.Canny(gray, 30, 150)
 cv2.imshow("Image", gray)
-#cv2.imshow("Edged", edged)
+cv2.imshow("Edged", edged)
+
 thresholding = cv2.threshold(gray, 230, 255, cv2.THRESH_BINARY_INV)[1]
 cv2.imshow("thresholding", thresholding)
 
@@ -28,4 +29,18 @@ text = "I found {} objects!".format(len(cnts))
 cv2.putText(output, text, (10,25), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (51, 51, 255), 2)
 cv2.imshow("Contours", output)
 
+#Erosions
+#apply erosions to reduce the size of foreground objects
+mask = thresholding.copy()
+mask = cv2.erode(mask, None, iterations=5)
+cv2.imshow("Eroded", mask)
+
+#Dilations
+mask = thresholding.copy()
+mask = cv2.dilate(mask, None, iterations=5)
+cv2.imshow("Dilated", mask)
+
+mask = thresholding.copy()
+output = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("Output", output)
 cv2.waitKey(0)
