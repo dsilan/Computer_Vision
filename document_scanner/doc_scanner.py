82871869162d4e52f8b0495ea53225ebe1907e5d
dsilan <dorsinsilan.isik@ogr.iu.edu.tr>
2020-01-05 20:14:18 +0300

    completed scanning

--- document_scanner/doc_scanner.py
+++ document_scanner/doc_scanner.py
@@ -1,7 +1,13 @@
 import cv2
 import imutils
+import numpy as np 
+from perspective.pyimagesearch.transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
 #Step1: Detect Edges
 image = cv2.imread("../../images/fis.jpg")
+orig = image.copy()
+ratio =  image.shape[0] / 500.0
 image = imutils.resize(image, height=500)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -28,6 +34,14 @@ for c in cnts:
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 cv2.imshow("Outline", image)
 
+#Step 3: Apply a Perspective Transform & Threshold
 
+warped = four_point_transform(orig, screenCnt.reshape(8, 2) * ratio)
+warped = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+warped = (warped > T).astype("uint8") * 255
+
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
